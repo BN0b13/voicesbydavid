fda18f94dc6a7dd9a676f49e83962ae887ca67b8
BN0b13 <NobleNotes1@gmail.com>
2023-10-01 19:28:10 -0700

    corrected config urls

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const themeTokenName = 'vbd-theme-token';
 
 // Prod
-// export const api = 'https://api.voicesbydavid.com';
-// export const url = 'https://www.voicesbydavid.com';
+export const api = 'https://api.voicesbydavid.com';
+export const url = 'https://www.voicesbydavid.com';
 
 // Local
-export const api = 'http://localhost:8050';
-export const url = 'http://localhost:3000';
+// export const api = 'http://localhost:8050';
+// export const url = 'http://localhost:3000';
