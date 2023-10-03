eed9dc2c6aaa2755da50a46385e7da92d5dd9036
BN0b13 <NobleNotes1@gmail.com>
2023-10-03 10:41:06 -0700

    commented out reels to test mobile

--- src/components/reels/reels.component.jsx
+++ src/components/reels/reels.component.jsx
@@ -8,13 +8,13 @@ const Reels = ({reels}) => {
     const dimensions = setMobileView() ? ["300", "205"] : ["560", "315"];
     return (
         <MainContainer>
-        {reels.map((reel, index) => (
+        {/* {reels.map((reel, index) => (
                 // <video key={index} controls width="250">
                 //     <source src={reel} type="video/webm" />
                 //     <source src={reel} type="video/mp4" />
                 // </video>
                 <iframe key={index} width={dimensions[0]} height={dimensions[1]} src={reel} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        ))}
+        ))} */}
         </MainContainer>
     )
 }
