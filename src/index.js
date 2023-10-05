ea83fcfedbbeee67329f47b8ac65c04cf98b2448
BN0b13 <NobleNotes1@gmail.com>
2023-10-05 10:17:31 -0700

    trying media query

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import App from './App';
 
 import './index.css';
 
-const vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-document.body.setAttribute(
-  'style',
-  `height: calc(var(--vh, 1vh) * 100);`
-);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
